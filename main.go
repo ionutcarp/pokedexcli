9a8a816d14924d55b87b81dcbff90ce2f1effe64
ionutcarp <carpionut@gmail.com>
2025-01-03 09:46:09 +0200

    CH1 L5

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	newScanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex> ")
-		newScanner.Scan()
-		text := newScanner.Text()
-		text = strings.ToLower(text)
-		words := strings.Fields(text)
-		fmt.Println("Your command was:", words[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,24 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex> ")
+		reader.Scan()
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+		fmt.Println("Your command was:", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
