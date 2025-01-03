4efc0ccf9f58fc3812ca3e11847d0cfbf5d0fd03
ionutcarp <carpionut@gmail.com>
2025-01-03 12:37:51 +0200

    CH1 L6

--- repl.go
+++ repl.go
@@ -7,6 +7,43 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	commands := getCommands()
+	for _, command := range commands {
+		fmt.Printf("  %s: %s\n", command.name, command.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
@@ -16,7 +53,15 @@ func startRepl() {
 		if len(words) == 0 {
 			continue
 		}
-		fmt.Println("Your command was:", words[0])
+		command, ok := getCommands()[words[0]]
+		if !ok {
+			fmt.Printf("Unknown command: %s\n", words[0])
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
 	}
 }
 
